ce3ab28f58e4ce4a4d337b44d9c4f7e409512013
Swaroop-751 <103578627+Swaroop-751@users.noreply.github.com>
2022-04-20 12:24:43 +0530

    Update handler.py

--- ec2-start-stop-lambda-cloudwatch-python/handler.py
+++ ec2-start-stop-lambda-cloudwatch-python/handler.py
@@ -27,7 +27,6 @@ def get_all_ec2_ids():
     instances = []
     for reservation in response["Reservations"]:
         for instance in reservation["Instances"]:
-            # This sample print will output entire Dictionary object
             # This will print will output the value of the Dictionary key 'InstanceId'
             instances.append(instance["InstanceId"])
     return instances
